e30efd81a7b9bf774a78ba85945ff567aee616b5
iJoseD <jcorrea@maando.com>
2021-12-09 16:47:45 -0500

    Update productos.php

--- layouts/productos.php
+++ layouts/productos.php
@@ -124,7 +124,7 @@
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="editarProductoLabel">Crear nuevo producto</h5>
+                <h5 class="modal-title" id="editarProductoLabel">Editar producto</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
