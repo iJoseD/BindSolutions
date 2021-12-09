90d8d534655ac5f18357f52f622324763305eb8c
iJoseD <jcorrea@maando.com>
2021-12-09 11:11:51 -0500

    Ajustes

--- dist/requireHead.php
+++ dist/requireHead.php
@@ -1,33 +1,31 @@
 <?php
-    if ( strpos( $_SERVER['REQUEST_URI'], '/payments/' ) !== false ) { } else {
-        session_start();
+    // if ( strpos( $_SERVER['REQUEST_URI'], '/payments/' ) !== false ) { } else {
+    //     session_start();
 
-        if ( empty( $_SESSION['phone'] ) ) {
-            if ( $_SERVER['REQUEST_URI'] != '/login/' ) {
-                header( 'location: ../login/' );
-            }
-        } else {
-            if ( $_SERVER['REQUEST_URI'] == '/login/' ) {
-                header( 'location: /' );
-            }
-        }
-    }
+    //     if ( empty( $_SESSION['phone'] ) ) {
+    //         if ( $_SERVER['REQUEST_URI'] != '/login/' ) {
+    //             header( 'location: ../login/' );
+    //         }
+    //     } else {
+    //         if ( $_SERVER['REQUEST_URI'] == '/login/' ) {
+    //             header( 'location: /' );
+    //         }
+    //     }
+    // }
 ?>
 
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
-    <link rel="icon" href="https://thankyoutipper.com/wp-content/uploads/2021/08/cropped-favicon2-32x32.jpg" sizes="32x32">
+    <!-- <link rel="icon" href="https://thankyoutipper.com/wp-content/uploads/2021/08/cropped-favicon2-32x32.jpg" sizes="32x32">
     <link rel="icon" href="https://thankyoutipper.com/wp-content/uploads/2021/08/cropped-favicon2-192x192.jpg" sizes="192x192">
     <link rel="apple-touch-icon" href="https://thankyoutipper.com/wp-content/uploads/2021/08/cropped-favicon2-180x180.jpg">
-    <meta name="msapplication-TileImage" content="https://thankyoutipper.com/wp-content/uploads/2021/08/cropped-favicon2-270x270.jpg">
+    <meta name="msapplication-TileImage" content="https://thankyoutipper.com/wp-content/uploads/2021/08/cropped-favicon2-270x270.jpg"> -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
     
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="/dist/css/intlTelInput.css">
     <link rel="stylesheet" href="/dist/css/style.css">
-    <link rel="stylesheet" href="/dist/css/menu.css">
     
     <title>Bind Solutions</title>
 </head>
--- layouts/navbar.php
+++ layouts/navbar.php
@@ -1,15 +1,26 @@
-<nav class="mt-3">
+<header class="p-3 bg-dark text-white">
     <div class="container">
-        <div class="row">
-            <div class="col-2">
-                <img src="/dist/img/menu.png" width="70%" alt="icoMenu" id="icoMenu">
+        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+            <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+                <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
+            </a>
+
+            <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                <li><a href="#" class="nav-link px-2 text-secondary">Home</a></li>
+                <li><a href="#" class="nav-link px-2 text-white">Features</a></li>
+                <li><a href="#" class="nav-link px-2 text-white">Pricing</a></li>
+                <li><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
+                <li><a href="#" class="nav-link px-2 text-white">About</a></li>
+            </ul>
+
+            <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+                <input type="search" class="form-control form-control-dark" placeholder="Search..." aria-label="Search" spellcheck="false" data-ms-editor="true">
+            </form>
+
+            <div class="text-end">
+                <button type="button" class="btn btn-outline-light me-2">Login</button>
+                <button type="button" class="btn btn-warning">Sign-up</button>
             </div>
-            <div class="col-8 text-center">
-                <a href="/"><img src="https://thankyoutipper.com/wp-content/uploads/2021/08/newLogo.svg" alt="Logo ThankYou Tipper" width="50%;"></a>
-            </div>
-            <div class="col-2"></div>
         </div>
     </div>
-</nav>
-
-<?php require_once('menu.php'); ?>
+</header>
