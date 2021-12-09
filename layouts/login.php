5454a882202651e2befd1616fa3e1d761384710a
iJoseD <jcorrea@maando.com>
2021-12-09 10:26:13 -0500

    Update login.php

--- layouts/login.php
+++ layouts/login.php
@@ -1,24 +1,24 @@
 <section class="container d-flex flex-column h-100 p-3">
     <div class="row mb-auto mt-auto bgLogin">
-        <div>
+        <div class="col-4"></div>
+        <div class="col-4">
             <div class="row">
-                <div>
-                    <figure class="iconotipo text-center">
-                        <img src="/dist/img/logoBind.png" alt="Logotipo">
-                    </figure>
+                <figure class="iconotipo text-center">
+                    <img src="/dist/img/logoBind.png" width="20%" alt="Logotipo">
+                </figure>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <input type="text" class="form-control" placeholder="First name" aria-label="First name">
+                </div>
+                <div class="col">
+                    <input type="text" class="form-control" placeholder="Last name" aria-label="Last name">
                 </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="col">
-                <input type="text" class="form-control" placeholder="First name" aria-label="First name">
-            </div>
-            <div class="col">
-                <input type="text" class="form-control" placeholder="Last name" aria-label="Last name">
+            <div class="d-grid mt-5">
+                <button type="button" id="btn--generateCode" class="btn btn-primary">Sign in</button>
             </div>
         </div>
-        <div class="d-grid mt-5">
-            <button type="button" id="btn--generateCode" class="btn btn-primary">Sign in</button>
-        </div>
+        <div class="col-4"></div>
     </div>
 </section>
