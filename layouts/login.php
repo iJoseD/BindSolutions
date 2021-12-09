d639a1916934a4d5f5300d308904bacae188e511
iJoseD <jcorrea@maando.com>
2021-12-09 10:34:47 -0500

    Update login.php

--- layouts/login.php
+++ layouts/login.php
@@ -5,16 +5,16 @@
             <figure class="iconotipo text-center">
                 <img src="/dist/img/logoBind.png" width="40%" alt="Logotipo">
             </figure>
-            <div class="row">
+            <div class="row mt-5">
                 <div class="col">
-                    <input type="text" class="form-control" placeholder="First name" aria-label="First name">
+                    <input type="text" name="user" id="user" class="form-control" placeholder="Usuario">
                 </div>
                 <div class="col">
-                    <input type="text" class="form-control" placeholder="Last name" aria-label="Last name">
+                    <input type="password" name="password" id="password" class="form-control" placeholder="Contraseña">
                 </div>
             </div>
             <div class="d-grid mt-5">
-                <button type="button" id="btn--generateCode" class="btn btn-primary">Sign in</button>
+                <button type="button" id="btn-iniciarSesion" class="btn btn-primary">Iniciar Sesión</button>
             </div>
         </div>
         <div class="col-3"></div>
