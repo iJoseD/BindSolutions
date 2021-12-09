ab4c75fabbabb74c1142ec0ea97964fd2ba85b5f
iJoseD <jcorrea@maando.com>
2021-12-09 15:29:29 -0500

    Update productos.php

--- layouts/productos.php
+++ layouts/productos.php
@@ -80,40 +80,26 @@
                                     <img class="card-img-top" src="/dist/img/empty.jpg">
                                     <div class="card-body d-grid">
                                         <input type="file" class="form-control" name="img--profile" id="img--profile">
-                                        <input type="button" class="btn btn-primary upload mt-3" value="Cargar">
+                                        <input type="button" class="btn btn-primary upload mt-3" value="Subir imagen">
                                     </div>
                                 </div>
                             </form>
                         </div>
-                        <div class="col-6">
-                            <label class="form-label">Nombre completo</label>
-                            <input type="text" name="fullName" id="fullName" class="form-control" placeholder="Joan Alonso Rivero">
-                        </div>
-                        <div class="col-6">
-                            <label class="form-label">Rol</label>
-                            <select name="rol" id="rol" class="form-select">
-                                <option selected>---</option>
-                                <?php
-                                    $sql = "SELECT * FROM rol";
-                                    $result = $conn->query($sql);
-                                    
-                                    if ($result->num_rows > 0) {
-                                        while($row = $result->fetch_assoc()) {
-                                            echo '<option value="'. $row['id'] .'">'. $row['rol'] .'</option>';
-                                        }
-                                    }
-                                ?>
-                            </select>
+                    </div>
+                    <div class="row mt-3">
+                        <div class="col-12">
+                            <label class="form-label">Nombre producto</label>
+                            <input type="text" name="nombre" id="nombre" class="form-control" placeholder="Old Parr 750ml">
                         </div>
                     </div>
                     <div class="row mt-3">
                         <div class="col-6">
-                            <label class="form-label">Usuario</label>
-                            <input type="text" name="user" id="user" class="form-control" placeholder="JoanAlonso">
+                            <label class="form-label">Costo</label>
+                            <input type="text" name="costo" id="costo" class="form-control" placeholder="90.000">
                         </div>
                         <div class="col-6">
-                            <label class="form-label">Contraseña</label>
-                            <input type="password" name="password" id="password" class="form-control" placeholder="Contraseña">
+                            <label class="form-label">Precio al público</label>
+                            <input type="text" name="precioPublico" id="precioPublico" class="form-control" placeholder="120.000">
                         </div>
                     </div>
                 </div>
@@ -121,14 +107,14 @@
                     <div class="row">
                         <div class="col-12 text-center">
                             <img src="/dist/img/tick.png" width="25%" alt="Tick">
-                            <h4 class="mt-4">Usuario creado correctamente</h4>
+                            <h4 class="mt-4">Producto creado correctamente</h4>
                         </div>
                     </div>
                 </div>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-                <button type="button" id="btn-crearUsuario" class="btn btn-primary">Crear usuario</button>
+                <button type="button" id="btn-crearUsuario" class="btn btn-primary">Crear producto</button>
             </div>
         </div>
     </div>
