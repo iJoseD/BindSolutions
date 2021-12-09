6bb28deb48119c2c93f80b01d5fdfdffe2fffc97
iJoseD <jcorrea@maando.com>
2021-12-09 10:31:51 -0500

    Ajustes login

--- layouts/login.php
+++ layouts/login.php
@@ -1,12 +1,10 @@
 <section class="container d-flex flex-column h-100 p-3">
-    <div class="row mb-auto mt-auto bgLogin">
-        <div class="col-4"></div>
-        <div class="col-4">
-            <div class="row">
-                <figure class="iconotipo text-center">
-                    <img src="/dist/img/logoBind.png" width="20%" alt="Logotipo">
-                </figure>
-            </div>
+    <div class="row mb-auto mt-auto">
+        <div class="col-3"></div>
+        <div class="col-6 bgLogin">
+            <figure class="iconotipo text-center">
+                <img src="/dist/img/logoBind.png" width="40%" alt="Logotipo">
+            </figure>
             <div class="row">
                 <div class="col">
                     <input type="text" class="form-control" placeholder="First name" aria-label="First name">
@@ -19,6 +17,6 @@
                 <button type="button" id="btn--generateCode" class="btn btn-primary">Sign in</button>
             </div>
         </div>
-        <div class="col-4"></div>
+        <div class="col-3"></div>
     </div>
 </section>
