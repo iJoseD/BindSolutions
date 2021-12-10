99a283bf0d5191e62a3d1b89a8679052fe0db49c
iJoseD <jcorrea@maando.com>
2021-12-10 09:46:48 -0500

    Update evento.php

--- layouts/evento.php
+++ layouts/evento.php
@@ -38,46 +38,49 @@
 <section class="container">
     <div class="row mt-5">
         <div class="col-3">
-            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Crear nuevo evento</button>
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#agregarInventario">Agregar inventario</button>
+        </div>
+        <div class="col-3">
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Agregar punto de venta</button>
         </div>
     </div>
 
     <div class="row mt-5">
-        <table id="tableEventos" class="display">
+        <h3>Inventario disponible</h3>
+        <table id="tableProductos" class="display">
             <thead>
                 <tr>
+                    <th>Imagen</th>
                     <th>Nombre</th>
-                    <th>Fecha</th>
-                    <th>Lugar</th>
+                    <th>Costo</th>
+                    <th>Precio al público</th>
+                    <th>Ganancia estimada</th>
+                    <th>Unidades disponibles</th>
                     <th>Opciones</th>
                 </tr>
             </thead>
-            <a href="http://"></a>
             <tbody>
                 <?php
-                    $sql = "SELECT * FROM eventos WHERE status = '1'";
+                    $sql = "SELECT * FROM productos WHERE status = '1'";
                     $result = $conn->query($sql);
                     
                     if ($result->num_rows > 0) {
                         while($row = $result->fetch_assoc()) {
                             $html = '<tr>';
+                                $html .= '<th><img src="'. $row['imagen'] .'" alt="'. $row['nombre'] .'" class="imgProducto"></th>';
                                 $html .= '<th>'. $row['nombre'] .'</th>';
-                                $html .= '<th>'. $row['fechaFormato'] .'</th>';
-                                $html .= '<th>'. $row['lugar'] .'</th>';
+                                $html .= '<th>'. $row['costo'] .'</th>';
+                                $html .= '<th>'. $row['precioPublico'] .'</th>';
+                                $html .= '<th>'. $row['precioPublico'] .'</th>';
+                                $html .= '<th>'. $row['precioPublico'] .'</th>';
                                 $html .= '<th>
-                                    <a href="/evento/?codigoEvento='. $row['codigoEvento'] .'" class="btn btn-success">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-gear" viewBox="0 0 16 16">
-                                            <path d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492zM5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0z"/>
-                                            <path d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52l-.094-.319zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115l.094-.319z"/>
-                                        </svg>
-                                    </a>
-                                    <button type="button" class="btn btn-warning editarEvento" data-bs-toggle="modal" data-bs-target="#editarEvento" data-id="'. $row['id'] .'" data-nombre="'. $row['nombre'] .'" data-fecha="'. $row['fecha'] .'" data-lugar="'. $row['lugar'] .'" data-codigoEvento="'. $row['codigoEvento'] .'">
+                                    <button type="button" class="btn btn-warning editarProducto" data-bs-toggle="modal" data-bs-target="#editarProducto" data-id="'. $row['id'] .'" data-imagen="'. $row['imagen'] .'" data-nombre="'. $row['nombre'] .'" data-costo="'. $row['costo'] .'" data-precioPublico="'. $row['precioPublico'] .'">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                                             <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
                                             <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
                                         </svg>
                                     </button>
-                                    <button type="button" class="btn btn-danger eliminarEvento" data-bs-toggle="modal" data-bs-target="#eliminarEvento" data-id="'. $row['id'] .'" data-nombre="'. $row['nombre'] .'">
+                                    <button type="button" class="btn btn-danger eliminarProducto" data-bs-toggle="modal" data-bs-target="#eliminarProducto" data-id="'. $row['id'] .'" data-nombre="'. $row['nombre'] .'">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-square" viewBox="0 0 16 16">
                                             <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
                                             <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
@@ -94,151 +97,6 @@
         </table>
     </div>
 </section>
-
-<!-- Crear evento -->
-<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="staticBackdropLabel">Crear nuevo evento</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <div class="formulario">
-                    <div class="row">
-                        <div class="col-12">
-                            <label class="form-label">Nombre</label>
-                            <input type="text" name="nombre" id="nombre" class="form-control" placeholder="Silvestre en Trucupey">
-                        </div>
-                    </div>
-                    <div class="row mt-3">
-                        <div class="col-6">
-                            <label class="form-label">Fecha</label>
-                            <input type="text" name="fecha" id="fecha" class="form-control datepicker" placeholder="<?php echo date("n/d/Y"); ?>">
-                        </div>
-                        <div class="col-6">
-                            <label class="form-label">Lugar</label>
-                            <input type="text" name="lugar" id="lugar" class="form-control" placeholder="Trucupey">
-                        </div>
-                    </div>
-                    <div class="row mt-3">
-                        <div class="col-12">
-                            <label class="form-label">Evento ID</label>
-                            <input type="text" name="codigoEvento" id="codigoEvento" class="form-control" value="<?php echo $code; ?>" readonly>
-                        </div>
-                    </div>
-                </div>
-                <div class="successful-message hide">
-                    <div class="row">
-                        <div class="col-12 text-center">
-                            <img src="/dist/img/tick.png" width="25%" alt="Tick">
-                            <h4 class="mt-4">Evento creado correctamente</h4>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-                <button type="button" id="btn-crearEvento" class="btn btn-primary">Crear evento</button>
-            </div>
-        </div>
-    </div>
-</div>
-
-<!-- Editar evento -->
-<div class="modal fade" id="editarEvento" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="editarEventoLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="editarEventoLabel">Editar evento</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <div class="formulario">
-                    <div class="row hide">
-                        <div class="col-12">
-                            <label class="form-label">Id evento</label>
-                            <input type="text" name="edit-id" id="edit-id" class="form-control" readonly>
-                        </div>
-                    </div>  
-                    <div class="row">
-                        <div class="col-12">
-                            <label class="form-label">Nombre</label>
-                            <input type="text" name="edit-nombre" id="edit-nombre" class="form-control" placeholder="Silvestre en Trucupey">
-                        </div>
-                    </div>
-                    <div class="row mt-3">
-                        <div class="col-6">
-                            <label class="form-label">Fecha</label>
-                            <input type="text" name="edit-fecha" id="edit-fecha" class="form-control datepicker" placeholder="<?php echo date("n/d/Y"); ?>">
-                        </div>
-                        <div class="col-6">
-                            <label class="form-label">Lugar</label>
-                            <input type="text" name="edit-lugar" id="edit-lugar" class="form-control" placeholder="Trucupey">
-                        </div>
-                    </div>
-                    <div class="row mt-3">
-                        <div class="col-12">
-                            <label class="form-label">Link para socios</label>
-                            <input type="text" name="edit-codigoEvento" id="edit-codigoEvento" class="form-control" readonly>
-                        </div>
-                    </div>
-                </div>
-                <div class="successful-message hide">
-                    <div class="row">
-                        <div class="col-12 text-center">
-                            <img src="/dist/img/tick.png" width="25%" alt="Tick">
-                            <h4 class="mt-4">Evento editado correctamente</h4>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-                <button type="button" id="btn-editarEvento" class="btn btn-warning">Editar evento</button>
-            </div>
-        </div>
-    </div>
-</div>
-
-<!-- Eliminar evento -->
-<div class="modal fade" id="eliminarEvento" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="eliminarEventoLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="eliminarEventoLabel">Eliminar evento</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <div class="formulario">
-                    <div class="row mt-3 hide">
-                        <div class="col-12">
-                            <label class="form-label">Id evento</label>
-                            <input type="text" name="delete-id" id="delete-id" class="form-control" readonly>
-                        </div>
-                    </div>    
-                    <div class="row">
-                        <div class="col-12 text-center">
-                            <h4>¿Esta seguro que desea eliminar el evento <span class="event"></span>?</h4>
-                        </div>
-                    </div>
-                </div>
-                <div class="successful-message hide">
-                    <div class="row">
-                        <div class="col-12 text-center">
-                            <img src="/dist/img/tick.png" width="25%" alt="Tick">
-                            <h4 class="mt-4">Evento eliminado correctamente</h4>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-                <button type="button" id="btn-eliminarEvento" class="btn btn-danger">Eliminar evento</button>
-            </div>
-        </div>
-    </div>
-</div>
 
 <!-- Agregar inventario -->
 <div class="modal fade" id="agregarInventario" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="agregarInventarioLabel" aria-hidden="true">
