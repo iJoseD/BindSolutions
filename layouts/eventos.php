d0460fea4f87cb1baba507fd4be56d9158a12579
iJoseD <jcorrea@maando.com>
2021-12-10 00:39:27 -0500

    evento

--- layouts/eventos.php
+++ layouts/eventos.php
@@ -9,7 +9,6 @@
     if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error); }
 
     $code = 'BSE' . rand(1000, 9999);
-    $linkSocio = 'https://' . $_SERVER[HTTP_HOST] . '/ver-evento/?evento=' . $code;
 ?>
 
 <section class="container">
@@ -26,7 +25,6 @@
                     <th>Nombre</th>
                     <th>Fecha</th>
                     <th>Lugar</th>
-                    <th>Link para Socios</th>
                     <th>Opciones</th>
                 </tr>
             </thead>
@@ -41,18 +39,11 @@
                                 $html .= '<th>'. $row['nombre'] .'</th>';
                                 $html .= '<th>'. $row['fechaFormato'] .'</th>';
                                 $html .= '<th>'. $row['lugar'] .'</th>';
-                                $html .= '<th>'. $row['linkSocio'] .'</th>';
                                 $html .= '<th>
-                                    <button type="button" class="btn btn-primary agregarInventario" data-bs-toggle="modal" data-bs-target="#agregarInventario" data-id="'. $row['id'] .'" data-nombre="'. $row['nombre'] .'">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
-                                            <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
-                                            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
-                                        </svg>
-                                    </button>
-                                    <button type="button" class="btn btn-success agregarPuntoVenta" data-bs-toggle="modal" data-bs-target="#agregarPuntoVenta" data-id="'. $row['id'] .'" data-nombre="'. $row['nombre'] .'">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
-                                            <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
-                                            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+                                    <button type="button" class="btn btn-success configurarEvento" data-linkSocio="'. $row['linkSocio'] .'">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-gear" viewBox="0 0 16 16">
+                                            <path d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492zM5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0z"/>
+                                            <path d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52l-.094-.319zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115l.094-.319z"/>
                                         </svg>
                                     </button>
                                     <button type="button" class="btn btn-warning editarEvento" data-bs-toggle="modal" data-bs-target="#editarEvento" data-id="'. $row['id'] .'" data-nombre="'. $row['nombre'] .'" data-fecha="'. $row['fecha'] .'" data-lugar="'. $row['lugar'] .'" data-linkSocio="'. $row['linkSocio'] .'">
@@ -107,8 +98,8 @@
                     </div>
                     <div class="row mt-3">
                         <div class="col-12">
-                            <label class="form-label">Link para socios</label>
-                            <input type="text" name="linkSocio" id="linkSocio" class="form-control" value="<?php echo $linkSocio; ?>" readonly>
+                            <label class="form-label">Evento ID</label>
+                            <input type="text" name="linkSocio" id="linkSocio" class="form-control" value="<?php echo $code; ?>" readonly>
                         </div>
                     </div>
                 </div>
