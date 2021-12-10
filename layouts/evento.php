50d50b8032312813536902f125c8f67c71e84ee8
iJoseD <jcorrea@maando.com>
2021-12-10 12:42:48 -0500

    Update evento.php

--- layouts/evento.php
+++ layouts/evento.php
@@ -68,13 +68,16 @@
                     
                     if ($result->num_rows > 0) {
                         while($row = $result->fetch_assoc()) {
+                            
+                            $ganancia = str_replace( '.', '', $row['precioPublico'] ) - str_replace( '.', '', $row['costo'] );
+                            
                             $html = '<tr>';
                                 $html .= '<th><img src="'. $row['imagen'] .'" alt="'. $row['nombre'] .'" class="imgProducto"></th>';
                                 $html .= '<th>'. $row['nombre'] .'</th>';
                                 $html .= '<th>'. $row['costo'] .'</th>';
                                 $html .= '<th>'. $row['precioPublico'] .'</th>';
-                                $html .= '<th>'. $row['precioPublico'] .'</th>';
-                                $html .= '<th>'. $row['precioPublico'] .'</th>';
+                                $html .= '<th>'. $ganancia .'</th>';
+                                $html .= '<th>'. $row['cantidad'] .'</th>';
                                 $html .= '<th>
                                     <button type="button" class="btn btn-warning editarProducto" data-bs-toggle="modal" data-bs-target="#editarProducto" data-id="'. $row['id'] .'" data-imagen="'. $row['imagen'] .'" data-nombre="'. $row['nombre'] .'" data-costo="'. $row['costo'] .'" data-precioPublico="'. $row['precioPublico'] .'">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
