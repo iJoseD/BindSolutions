a4fd4879c55bd7adbb5abadefaff29785b51a10c
iJoseD <jcorrea@maando.com>
2021-12-10 20:41:56 -0500

    Update usuarios.php

--- layouts/usuarios.php
+++ layouts/usuarios.php
@@ -124,7 +124,7 @@
                         <div class="col-6">
                             <label class="form-label">Evento</label>
                             <select name="crearUsuario-SelectEvento" id="crearUsuario-SelectEvento" class="form-select">
-                                <option selected>---</option>
+                                <option value="0">---</option>
                                 <?php
                                     $sql = "SELECT * FROM eventos ORDER BY nombre ASC";
                                     $result = $conn->query($sql);
@@ -140,7 +140,7 @@
                         <div class="col-6">
                             <label class="form-label">Punto de venta</label>
                             <select name="crearUsuario-SelectPV" id="crearUsuario-SelectPV" class="form-select">
-                                <option selected>---</option>
+                                <option value="0">---</option>
                             </select>
                         </div>
                     </div>
