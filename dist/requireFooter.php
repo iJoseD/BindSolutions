cbb6c1847baa0e9ac8d79ff515766d08d8b1deba
iJoseD <jcorrea@maando.com>
2021-12-10 19:36:12 -0500

    Update requireFooter.php

--- dist/requireFooter.php
+++ dist/requireFooter.php
@@ -1,3 +1,20 @@
+<div class="container">
+    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        <p class="col-md-4 mb-0 text-muted">© <?php echo date('Y'); ?> Bind Solutions. Desarrollado por Jose Correa.</p>
+
+        <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+            <img src="/dist/img/logoBind.png" width="20%" alt="Logotipo">
+        </a>
+
+        <ul class="nav col-md-4 justify-content-end">
+            <li class="nav-item"><a href="/" class="nav-link px-2 text-muted">Dashboard</a></li>
+            <li class="nav-item"><a href="/productos" class="nav-link px-2 text-muted">Productos</a></li>
+            <li class="nav-item"><a href="/eventos" class="nav-link px-2 text-muted">Eventos</a></li>
+            <li class="nav-item"><a href="/usuarios" class="nav-link px-2 text-muted">Usuarios</a></li>
+        </ul>
+    </footer>
+</div>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/ui/1.13.0/jquery-ui.js"></script>
