465f87c6b6f9fdeabc6ca849122be2e88d4f381e
iJoseD <jcorrea@maando.com>
2021-12-13 12:24:37 -0500

    Update navbar.php

--- layouts/navbar.php
+++ layouts/navbar.php
@@ -3,8 +3,7 @@
         <div class="row">
             <div class="col-xl-6 col-md-12 col-12">
                 <a href="/" class="text-decoration-none d-xl-none d-md-none d-block"><img src="/dist/img/logoBind.png" width="80%" alt="Logotipo"></a>
-                <a href="/" class="text-decoration-none d-xl-block d-md-none d-none"><img src="/dist/img/logoBind.png" width="20%" alt="Logotipo"></a>
-                <a href="/" class="text-decoration-none d-xl-none d-md-block d-none"><img src="/dist/img/logoBind.png" width="35%" alt="Logotipo"></a>
+                <a href="/" class="text-decoration-none d-xl-block d-md-block d-none"><img src="/dist/img/logoBind.png" width="20%" alt="Logotipo"></a>
             </div>
 
             <!-- Mobile -->
@@ -43,15 +42,6 @@
                                 <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
                             </svg>
                             <span class="mt-1" style="font-size: small;">Usuarios</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/logout" class="nav-link text-white text-center d-flex">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
-                                <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/>
-                                <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
-                            </svg>
-                            <span class="mt-1" style="font-size: small;">Cerrar sesión</span>
                         </a>
                     </li>
                 </ul>
