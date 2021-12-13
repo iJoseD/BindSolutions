6a39bc90160f7671ce2dd5644ea516dba6a5583a
iJoseD <jcorrea@maando.com>
2021-12-13 09:33:07 -0500

    Update script.js

--- dist/js/script.js
+++ dist/js/script.js
@@ -38,11 +38,7 @@ $('#btn-iniciarSesion').click(function() {
 $(document).ready(function () {
     $('.DataTable').DataTable({
         language: { url: "//cdn.datatables.net/plug-ins/1.10.22/i18n/Spanish.json" },
-        responsive: true,
-        columnDefs: [
-            { responsivePriority: 1, targets: 0 },
-            { responsivePriority: 2, targets: -1 }
-        ]
+        responsive: true
     });
 
     $( ".datepicker" ).datepicker();
