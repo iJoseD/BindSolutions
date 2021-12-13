b0a6e1f218e2aefd97d021c49160c3b4219de639
iJoseD <jcorrea@maando.com>
2021-12-13 12:29:56 -0500

    Update navbar.php

--- layouts/navbar.php
+++ layouts/navbar.php
@@ -1,13 +1,13 @@
 <header class="container-fluid bg-dark p-3">
     <div class="container">
         <div class="row">
-            <div class="col-xl-6 col-md-12 col-12">
+            <div class="col-xl-6 col-md-4 col-12">
                 <a href="/" class="text-decoration-none text-center d-xl-none d-md-none d-block"><img src="/dist/img/logoBind.png" width="35%" alt="Logotipo"></a>
                 <a href="/" class="text-decoration-none d-xl-block d-md-block d-none"><img src="/dist/img/logoBind.png" width="20%" alt="Logotipo"></a>
             </div>
 
             <!-- Mobile -->
-            <div class="col-xl-6 col-md-12 col-12 mt-3 d-xl-none d-md-none d-block">
+            <div class="col-12 mt-3 d-xl-none d-md-none d-block">
                 <ul class="col-lg-auto nav text-small justify-content-center">
                     <li>
                         <a href="/" class="nav-link text-white text-center d-flex">
@@ -47,8 +47,58 @@
                 </ul>
             </div>
 
+            <!-- Tablet -->
+            <div class="col-md-8 d-xl-none d-md-block d-none">
+                <ul class="col-lg-auto nav text-small justify-content-end">
+                    <li>
+                        <a href="/" class="nav-link text-white text-center d-flex">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-speedometer2" viewBox="0 0 16 16">
+                                <path d="M8 4a.5.5 0 0 1 .5.5V6a.5.5 0 0 1-1 0V4.5A.5.5 0 0 1 8 4zM3.732 5.732a.5.5 0 0 1 .707 0l.915.914a.5.5 0 1 1-.708.708l-.914-.915a.5.5 0 0 1 0-.707zM2 10a.5.5 0 0 1 .5-.5h1.586a.5.5 0 0 1 0 1H2.5A.5.5 0 0 1 2 10zm9.5 0a.5.5 0 0 1 .5-.5h1.5a.5.5 0 0 1 0 1H12a.5.5 0 0 1-.5-.5zm.754-4.246a.389.389 0 0 0-.527-.02L7.547 9.31a.91.91 0 1 0 1.302 1.258l3.434-4.297a.389.389 0 0 0-.029-.518z"/>
+                                <path fill-rule="evenodd" d="M0 10a8 8 0 1 1 15.547 2.661c-.442 1.253-1.845 1.602-2.932 1.25C11.309 13.488 9.475 13 8 13c-1.474 0-3.31.488-4.615.911-1.087.352-2.49.003-2.932-1.25A7.988 7.988 0 0 1 0 10zm8-7a7 7 0 0 0-6.603 9.329c.203.575.923.876 1.68.63C4.397 12.533 6.358 12 8 12s3.604.532 4.923.96c.757.245 1.477-.056 1.68-.631A7 7 0 0 0 8 3z"/>
+                            </svg>
+                            <span class="mt-1" style="font-size: small;">Dashboard</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/productos" class="nav-link text-white text-center d-flex">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-box-seam" viewBox="0 0 16 16">
+                                <path d="M8.186 1.113a.5.5 0 0 0-.372 0L1.846 3.5l2.404.961L10.404 2l-2.218-.887zm3.564 1.426L5.596 5 8 5.961 14.154 3.5l-2.404-.961zm3.25 1.7-6.5 2.6v7.922l6.5-2.6V4.24zM7.5 14.762V6.838L1 4.239v7.923l6.5 2.6zM7.443.184a1.5 1.5 0 0 1 1.114 0l7.129 2.852A.5.5 0 0 1 16 3.5v8.662a1 1 0 0 1-.629.928l-7.185 2.874a.5.5 0 0 1-.372 0L.63 13.09a1 1 0 0 1-.63-.928V3.5a.5.5 0 0 1 .314-.464L7.443.184z"/>
+                            </svg>
+                            <span class="mt-1" style="font-size: small;">Productos</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/eventos" class="nav-link text-white text-center d-flex">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-calendar-date" viewBox="0 0 16 16">
+                                <path d="M6.445 11.688V6.354h-.633A12.6 12.6 0 0 0 4.5 7.16v.695c.375-.257.969-.62 1.258-.777h.012v4.61h.675zm1.188-1.305c.047.64.594 1.406 1.703 1.406 1.258 0 2-1.066 2-2.871 0-1.934-.781-2.668-1.953-2.668-.926 0-1.797.672-1.797 1.809 0 1.16.824 1.77 1.676 1.77.746 0 1.23-.376 1.383-.79h.027c-.004 1.316-.461 2.164-1.305 2.164-.664 0-1.008-.45-1.05-.82h-.684zm2.953-2.317c0 .696-.559 1.18-1.184 1.18-.601 0-1.144-.383-1.144-1.2 0-.823.582-1.21 1.168-1.21.633 0 1.16.398 1.16 1.23z"></path>
+                                <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z"></path>
+                            </svg>
+                            <span class="mt-1" style="font-size: small;">Eventos</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/usuarios" class="nav-link text-white text-center d-flex">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                                <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+                            </svg>
+                            <span class="mt-1" style="font-size: small;">Usuarios</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/logout" class="nav-link text-white text-center d-flex">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/>
+                                <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
+                            </svg>
+                            <span class="mt-1" style="font-size: small;">Cerrar sesión</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
             <!-- Desktop -->
-            <div class="col-xl-6 col-md-12 col-12 d-xl-block d-md-block d-none">
+            <div class="col-xl-6 d-xl-block d-md-none d-none">
                 <ul class="col-lg-auto nav text-small justify-content-end">
                     <li>
                         <a href="/" class="nav-link text-white text-center d-flex">
