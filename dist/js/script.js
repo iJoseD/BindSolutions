dab353c3d5bb4c85ea455debafbe0e6a7b112ff6
iJoseD <jcorrea@maando.com>
2021-12-16 00:27:07 -0500

    Solo números

--- dist/js/script.js
+++ dist/js/script.js
@@ -1218,3 +1218,13 @@ function separadorMiles(donde, caracter) {
         donde.value = cad2
     }
 }
+
+// Solo números
+$('#cantidad, #agregarPuntoVenta-cantMesas, #ei-cantidad, #editarPuntoV-cantMesas, #SubInventario-Cantidad, #editarSubInventario-Cantidad').bind('keypress', function(event) {
+    var regex = new RegExp("^[0-9]+$");
+    var key = String.fromCharCode(!event.charCode ? event.which : event.charCode);
+    if (!regex.test(key)) {
+        event.preventDefault();
+        return false;
+    }
+});
