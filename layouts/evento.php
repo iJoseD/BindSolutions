c9a8d64cdf45432b8814826fcbc15850c9793352
iJoseD <jcorrea@maando.com>
2021-12-18 10:56:31 -0500

    Update evento.php

--- layouts/evento.php
+++ layouts/evento.php
@@ -144,18 +144,19 @@
                                     $html .= '<th>'. $row['nombre'] .'</th>';
                                     $html .= '<th>'. $row['cantMesas'] .'</th>';
                                     $html .= '<th>
-                                        <button type="button" class="btn btn-success agregarSubInventario" data-bs-toggle="modal" data-bs-target="#agregarSubInventario" data-idEvento="'. $id .'" data-idPV="'. $row['id'] .'">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
-                                                <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"/>
+                                        <button type="button" class="btn btn-success agregarSubInventario" data-bs-toggle="modal" data-bs-target="#agregarSubInventario" data-bs-idEvento="'. $id .'" data-bs-idPV="'. $row['id'] .'">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-tags" viewBox="0 0 16 16">
+                                                <path d="M3 2v4.586l7 7L14.586 9l-7-7H3zM2 2a1 1 0 0 1 1-1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 2 6.586V2z"/>
+                                                <path d="M5.5 5a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1zm0 1a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zM1 7.086a1 1 0 0 0 .293.707L8.75 15.25l-.043.043a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 0 7.586V3a1 1 0 0 1 1-1v5.086z"/>
                                             </svg>
                                         </button>
-                                        <button type="button" class="btn btn-warning editarPuntoV" data-bs-toggle="modal" data-bs-target="#editarPuntoV" data-id="'. $row['id'] .'" data-nombre="'. $row['nombre'] .'" data-cantMesas="'. $row['cantMesas'] .'">
+                                        <button type="button" class="btn btn-warning editarPuntoV" data-bs-toggle="modal" data-bs-target="#editarPuntoV" data-bs-id="'. $row['id'] .'" data-bs-nombre="'. $row['nombre'] .'" data-bs-cantMesas="'. $row['cantMesas'] .'">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                                                 <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
                                                 <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
                                             </svg>
                                         </button>
-                                        <button type="button" class="btn btn-danger eliminarPuntoV" data-bs-toggle="modal" data-bs-target="#eliminarPuntoV" data-id="'. $row['id'] .'" data-nombre="'. $row['nombre'] .'">
+                                        <button type="button" class="btn btn-danger eliminarPuntoV" data-bs-toggle="modal" data-bs-target="#eliminarPuntoV" data-bs-id="'. $row['id'] .'" data-bs-nombre="'. $row['nombre'] .'">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-square" viewBox="0 0 16 16">
                                                 <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
                                                 <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
