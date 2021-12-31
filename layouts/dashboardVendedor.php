bf8788000b4c982d369dae78e3c1b8696aebd82a
iJoseD <jcorrea@maando.com>
2021-12-30 19:02:20 -0500

    editarFactura

--- layouts/dashboardVendedor.php
+++ layouts/dashboardVendedor.php
@@ -152,10 +152,14 @@
                                     $html .= '<th>'. $row['cantidad'] .'</th>';
                                     $html .= '<th>$ '. number_format( $row['total'], 0, ',', '.' ) .'</th>';
                                     $html .= '<th>
-                                        <button type="button" class="btn btn-warning verFactura" data-bs-toggle="modal" data-bs-target="#verFactura" data-bs-codeFac="'. $row['codeFac'] .'">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag-check" viewBox="0 0 16 16">
-                                                <path fill-rule="evenodd" d="M10.854 8.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L7.5 10.793l2.646-2.647a.5.5 0 0 1 .708 0z"/>
-                                                <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z"/>
+                                        <button type="button" class="btn btn-success verFactura" data-bs-toggle="modal" data-bs-target="#verFactura" data-bs-codeFac="'. $row['codeFac'] .'">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag-check-fill" viewBox="0 0 16 16">
+                                                <path fill-rule="evenodd" d="M10.5 3.5a2.5 2.5 0 0 0-5 0V4h5v-.5zm1 0V4H15v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V4h3.5v-.5a3.5 3.5 0 1 1 7 0zm-.646 5.354a.5.5 0 0 0-.708-.708L7.5 10.793 6.354 9.646a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0l3-3z"/>
+                                            </svg>
+                                        </button>
+                                        <button type="button" class="btn btn-warning editarFactura" data-bs-toggle="modal" data-bs-target="#verFactura" data-bs-codeFac="'. $row['codeFac'] .'">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag-dash-fill" viewBox="0 0 16 16">
+                                                <path fill-rule="evenodd" d="M10.5 3.5a2.5 2.5 0 0 0-5 0V4h5v-.5zm1 0V4H15v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V4h3.5v-.5a3.5 3.5 0 1 1 7 0zM6 9.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1H6z"/>
                                             </svg>
                                         </button>
                                     </th>';
@@ -341,6 +345,46 @@
                     </div>
                 </div>
                 <div class="formulario">
+                    <div class="row mt-3 detalleFactura"></div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Ver venta -->
+<div class="modal fade" id="editarFactura" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="editarFacturaLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="editarFacturaLabel">Factura <span class="codeFac badge bg-primary"></span></h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="pin">
+                    <div class="row">
+                        <div class="col-10">
+                            <input type="password" name="editarFactura-Pin" id="editarFactura-Pin" class="form-control form-control-lg" placeholder="PIN DE SEGURIDAD">
+                        </div>
+                        <div class="col-2 d-grid">
+                            <button type="button" class="btn btn-lg bg-bind-1 fw-bold text-uppercase text-white">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2-circle" viewBox="0 0 16 16">
+                                    <path d="M2.5 8a5.5 5.5 0 0 1 8.25-4.764.5.5 0 0 0 .5-.866A6.5 6.5 0 1 0 14.5 8a.5.5 0 0 0-1 0 5.5 5.5 0 1 1-11 0z"/>
+                                    <path d="M15.354 3.354a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l7-7z"/>
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div class="formulario hide">
+                    <div class="mb-2 mt-2 row">
+                        <div class="col-12 text-center">
+                            <p class="badge bg-success text-uppercase text-white">Resumen de la venta</p>
+                        </div>
+                    </div>
                     <div class="row mt-3 detalleFactura"></div>
                 </div>
             </div>
