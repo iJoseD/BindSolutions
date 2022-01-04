f2a6f07e498b5d624ac90739aa78c9ee037ec2d7
iJoseD <jcorrea@maando.com>
2022-01-04 16:43:40 -0500

    crearEvento

--- eventos/index.php
+++ eventos/index.php
@@ -17,6 +17,10 @@
         <!-- Bootstrap -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
+        <!-- DataTables -->
+        <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
+        <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/responsive/2.2.9/js/dataTables.responsive.min.js"></script>
+
         <!-- App -->
         <script type="text/javascript" src="/dist/js/eventos.js"></script>
     </body>
